d03a49b2b99233fccda74f120b8848ca79e9eddf
Jan <eleventep@gmail.com>
2022-07-19 19:16:44 +0200

    readded count

--- Tasks/NameHandler.cs
+++ Tasks/NameHandler.cs
@@ -11,7 +11,10 @@ namespace UbisoftName.Tasks;
 internal static class NameHandler
 {
     private static UbisoftToken? _token;
-
+    
+    private static uint _namesChecked;
+    private static uint _namesAvailable;
+    
     internal static async Task CheckForNames()
     {
         if (_token == null || _token.IsExpired) _token = await GetToken();
@@ -19,34 +22,43 @@ internal static class NameHandler
         var namesToCheck = await File.ReadAllLinesAsync("names.txt");
         const string resultingFilePath = "availableNames.txt";
 
-        foreach (var name in namesToCheck)
+        if (namesToCheck.Length > 0)
         {
-            var request = new HttpRequestMessage
+            foreach (var name in namesToCheck)
             {
-                Method = HttpMethod.Get,
-                RequestUri =
-                    new Uri($"https://public-ubiservices.ubi.com/v3/profiles?nameOnPlatform={name}&platformType=uplay"),
-                Headers = { Authorization = new AuthenticationHeaderValue("Ubi_v1", $"t={_token?.Ticket}") }
-            };
+                var request = new HttpRequestMessage
+                {
+                    Method = HttpMethod.Get,
+                    RequestUri =
+                        new Uri($"https://public-ubiservices.ubi.com/v3/profiles?nameOnPlatform={name}&platformType=uplay"),
+                    Headers = { Authorization = new AuthenticationHeaderValue("Ubi_v1", $"t={_token?.Ticket}") }
+                };
 
-            var response = await GetHttpClient().SendAsync(request);
-            var content = JsonConvert.DeserializeObject<UbisoftProfile?>(await response.Content.ReadAsStringAsync());
+                var response = await GetHttpClient().SendAsync(request);
+                var content = JsonConvert.DeserializeObject<UbisoftProfile?>(await response.Content.ReadAsStringAsync());
 
-
-            if ((bool)content?.Profiles.Any())
-            {
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.WriteLine($"{name}");
-                Console.ResetColor();
-            }
-            else
-            {
-                Console.ForegroundColor = ConsoleColor.Green;
-                Console.WriteLine($"{name}");
-                Console.ResetColor();
-                await File.AppendAllLinesAsync(resultingFilePath, new[] { name + "(Available or Restricted)" });
+                if ((bool)content?.Profiles.Any())
+                {
+                    _namesChecked += 1;
+                    Console.Title = $"Checked: {_namesChecked} - Available: {_namesAvailable}";
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine($"{name}");
+                    Console.ResetColor();
+                }
+                else
+                {
+                    _namesChecked += 1;
+                    _namesAvailable += 1;
+                    Console.Title = $"Checked: {_namesChecked} - Available: {_namesAvailable}";
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine($"{name}");
+                    Console.ResetColor();
+                    await File.AppendAllLinesAsync(resultingFilePath, new[] { name + "(Available or Restricted)" });
+                }
             }
         }
+        else 
+            Console.WriteLine("Please enter names into names.txt like this: \nDigneety\nDignity");
     }
 
     private static async Task<UbisoftToken?> GetToken()
@@ -64,9 +76,10 @@ internal static class NameHandler
                 "application/json")
         };
         var response = await GetHttpClient().SendAsync(request);
-        if (!response.IsSuccessStatusCode)
-            throw new UnauthorizedAccessException("Unable to retrieve authentication ticket.");
-        return JsonConvert.DeserializeObject<UbisoftToken?>(await response.Content.ReadAsStringAsync())!;
+        if (response.IsSuccessStatusCode)
+            return JsonConvert.DeserializeObject<UbisoftToken?>(await response.Content.ReadAsStringAsync())!;
+        Console.ReadKey(true);
+        throw new UnauthorizedAccessException("Unable to retrieve authentication ticket.");
     }
 
     private static HttpClient GetHttpClient()
