0153c3178a0cab3005cc9dfce1589b74d1a9a24a
Jan <eleventep@gmail.com>
2022-07-21 09:46:17 +0200

    improved error handling

--- Tasks/NameHandler.cs
+++ Tasks/NameHandler.cs
@@ -11,54 +11,62 @@ namespace UbisoftName.Tasks;
 internal static class NameHandler
 {
     private static UbisoftToken? _token;
-    
+
     private static uint _namesChecked;
     private static uint _namesAvailable;
-    
+
     internal static async Task CheckForNames()
     {
         if (_token == null || _token.IsExpired) _token = await GetToken();
 
-        var namesToCheck = await File.ReadAllLinesAsync("names.txt");
-        const string resultingFilePath = "availableNames.txt";
-
-        if (namesToCheck.Length > 0)
+        try
         {
-            foreach (var name in namesToCheck)
+            var namesToCheck = await File.ReadAllLinesAsync("names.txt");
+            const string resultingFilePath = "availableNames.txt";
+            if (namesToCheck.Length > 0)
             {
-                var request = new HttpRequestMessage
+                foreach (var name in namesToCheck)
                 {
-                    Method = HttpMethod.Get,
-                    RequestUri =
-                        new Uri($"https://public-ubiservices.ubi.com/v3/profiles?nameOnPlatform={name}&platformType=uplay"),
-                    Headers = { Authorization = new AuthenticationHeaderValue("Ubi_v1", $"t={_token?.Ticket}") }
-                };
+                    var request = new HttpRequestMessage
+                    {
+                        Method = HttpMethod.Get,
+                        RequestUri =
+                            new Uri(
+                                $"https://public-ubiservices.ubi.com/v3/profiles?nameOnPlatform={name}&platformType=uplay"),
+                        Headers = { Authorization = new AuthenticationHeaderValue("Ubi_v1", $"t={_token?.Ticket}") }
+                    };
+                    var response = await GetHttpClient().SendAsync(request);
+                    var content =
+                        JsonConvert.DeserializeObject<UbisoftProfile?>(await response.Content.ReadAsStringAsync());
 
-                var response = await GetHttpClient().SendAsync(request);
-                var content = JsonConvert.DeserializeObject<UbisoftProfile?>(await response.Content.ReadAsStringAsync());
-
-                if ((bool)content?.Profiles.Any())
-                {
-                    _namesChecked += 1;
-                    Console.Title = $"Checked: {_namesChecked} - Available: {_namesAvailable}";
-                    Console.ForegroundColor = ConsoleColor.Red;
-                    Console.WriteLine($"{name}");
-                    Console.ResetColor();
-                }
-                else
-                {
-                    _namesChecked += 1;
-                    _namesAvailable += 1;
-                    Console.Title = $"Checked: {_namesChecked} - Available: {_namesAvailable}";
-                    Console.ForegroundColor = ConsoleColor.Green;
-                    Console.WriteLine($"{name}");
-                    Console.ResetColor();
-                    await File.AppendAllLinesAsync(resultingFilePath, new[] { name + "(Available or Restricted)" });
+                    if ((bool)content?.Profiles.Any())
+                    {
+                        _namesChecked += 1;
+                        Console.Title = $"Checked: {_namesChecked} - Available: {_namesAvailable}";
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine($"{name}");
+                        Console.ResetColor();
+                    }
+                    else
+                    {
+                        _namesChecked += 1;
+                        _namesAvailable += 1;
+                        Console.Title = $"Checked: {_namesChecked} - Available: {_namesAvailable}";
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.WriteLine($"{name}");
+                        Console.ResetColor();
+                        await File.AppendAllLinesAsync(resultingFilePath, new[] { name + " (Available or Restricted)" });
+                    }
                 }
             }
+            else
+                Console.WriteLine("Please enter names into names.txt like this: \nDigneety\nDignity");
         }
-        else 
-            Console.WriteLine("Please enter names into names.txt like this: \nDigneety\nDignity");
+        catch (FileNotFoundException)
+        {
+            Console.WriteLine(
+                "Please create a file called names.txt and then enter names into it like this: \nDigneety\nDignity");
+        }
     }
 
     private static async Task<UbisoftToken?> GetToken()
@@ -76,10 +84,14 @@ internal static class NameHandler
                 "application/json")
         };
         var response = await GetHttpClient().SendAsync(request);
-        if (response.IsSuccessStatusCode)
-            return JsonConvert.DeserializeObject<UbisoftToken?>(await response.Content.ReadAsStringAsync())!;
-        Console.ReadKey(true);
-        throw new UnauthorizedAccessException("Unable to retrieve authentication ticket.");
+        switch (response.IsSuccessStatusCode)
+        {
+            case true:
+                return JsonConvert.DeserializeObject<UbisoftToken?>(await response.Content.ReadAsStringAsync())!;
+            default:
+                Console.ReadKey(true);
+                throw new UnauthorizedAccessException("Unable to retrieve authentication ticket.");
+        }
     }
 
     private static HttpClient GetHttpClient()
